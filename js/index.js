e29329558e9d4deb5f9cd533abcb50ea80d7f1f7
stubrew24 <sewelly@gmail.com>
2019-03-13 11:59:59 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
+
+function save_lincoln(){
+  button = document.getElementById('save_lincoln')
+  image = document.getElementById('foreground')
+
+  button.addEventListener('click', function(event){
+    image.style.WebkitTransition = 'opacity 1s';
+    image.style.MozTransition = 'opacity 1s';
+    image.style.transition = 'opacity 1s'
+  })
 }
+
+save_lincoln()
